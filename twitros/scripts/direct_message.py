94e4de924d69c6c8ed3f9b977844f32fd8e99965
namimi <tronche.adrien@gmail.com>
2013-04-19 18:02:34 -0400

    Scripts to try posting tweets and dm along images.

--- twitros/scripts/direct_message.py
+++ twitros/scripts/direct_message.py
@@ -11,29 +11,30 @@ from cv_bridge import CvBridge, CvBridgeError
 from twitros_msgs.srv import *
 
 if __name__ == '__main__' :
-    if len( sys.argv ) > 1:
-        status = sys.argv[1]
+    if len( sys.argv ) > 2:
+        user = sys.argv[1]
+        msg = sys.argv[2]
 	paths = []
-        for i in range(2, len( sys.argv )):
+        for i in range(3, len( sys.argv )):
             paths.append(sys.argv[i])
     else:
         print("Usage: " + sys.argv[0] 
-	    + ' "MESSAGE" PATH_IMAGE_1 PATH_IMAGE_2 ...')
+	    + ' SCREEN_NAME "MESSAGE" PATH_IMAGE_1 PATH_IMAGE_2 ...')
         sys.exit(1)
 
-    rospy.wait_for_service('post_tweet')
-    tweet = rospy.ServiceProxy('post_tweet', Post)
+    rospy.wait_for_service('post_dm')
+    direct_message = rospy.ServiceProxy('post_dm', DirectMessage)
     bridge = CvBridge()
    
     # Convert pics to sensor_msgs/Image
     images = []
     for path in paths:
         try:
-            images.append( bridge.cv_to_imgmsg( cv.LoadImage(path) ) )
+            images.append( bridge.cv_to_imgmsg( cv.LoadImage(path) ) )    
         except CvBridgeError, e:
     	    rospy.logerr( "CvBridge error: " + e )
-	    
+
     try:
-        tweet( text = status, images = images )
+        direct_message( user = user, text = msg, images = images )
     except rospy.ServiceException, e:
         rospy.logerr( "Service call failed: " + e )
