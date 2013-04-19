3174f0442cc4487af3b9b475d438f3fdef13e042
namimi <tronche.adrien@gmail.com>
2013-04-19 18:05:09 -0400

    Big update. All features supposed to work. Fixed OAuth. Use requests on postimage.net to send multiple images in tweets and dm.

--- twitros/src/twitter.py
+++ twitros/src/twitter.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
 import os
-import sys
-from time import time, mktime, strftime, strptime
+from time import time, mktime, strptime
 
 # ROS
 import roslib; roslib.load_manifest('twitros')
@@ -12,7 +11,6 @@ import rospy
 from twython import Twython
 
 # To manage pictures
-import urllib
 import cv
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -20,9 +18,13 @@ from cv_bridge import CvBridge, CvBridgeError
 from twitros_msgs.msg import *
 from twitros_msgs.srv import *
 
+# Direct message image hosting
+import requests
+from bs4 import BeautifulSoup
+
+
 class TwitterServer:
     def __init__(self):
-
         # You can choose to latch tweets topics
 	latch = False
 	if rospy.has_param('latch'):
@@ -30,9 +32,9 @@ class TwitterServer:
 
 	# In case you can't direct message a user, replace DM with a public
 	#	'@user text' tweet.
-	replace_dm = False
+	self.replace_dm = False
 	if rospy.has_param('replace_dm'):
-	    replace_dm = rospy.get_param('replace_dm')
+	    self.replace_dm = rospy.get_param('replace_dm')
     	
 	# Publish mentions, home timeline and direct messages
         self.pub_home = rospy.Publisher('home_timeline', Tweets, latch = latch)
@@ -55,39 +57,40 @@ class TwitterServer:
 	    oauth_token = rospy.get_param('~token')
 	if rospy.has_param('~token_secret'):
 	    oauth_token_secret = rospy.get_param('~token_secret')
-	    
 
 	# OAuth token creation (see get_access_token.py from python-twitter)
 	if oauth_token is None or oauth_token_secret is None:
-	    rospy.loginfo('No OAuth information given, trying to create...')
+	    rospy.loginfo("No OAuth information given, trying to create...")
 
 	    t = Twython( app_key =  'HbAfkrfiw0s7Es4TVrpSuw',
                 app_secret = 'oIjEOsEbHprUa7EOi3Mo8rNBdQlHjTGPEpGrItZj8c')
 	
 	    # Get AUth URL. Use for login for security, 
-	    # screen_name can be on rosparam
-	    if rospy.has_param('~screen_name'):
-	    	sn = rospy.get_param('~screen_name')
-	    	url = t.get_authentication_tokens( 
-	    		screen_name = sn, force_login = True)
-	    else:
-	    	url = t.get_authentication_tokens( force_login = True )
+	    url = t.get_authentication_tokens( )
+
+	    t = Twython(app_key = 'HbAfkrfiw0s7Es4TVrpSuw',
+            	app_secret = 'oIjEOsEbHprUa7EOi3Mo8rNBdQlHjTGPEpGrItZj8c',
+            	oauth_token = url['oauth_token'],
+            	oauth_token_secret = url['oauth_token_secret'])
 
 	    # Open web browser on given url
 	    import webbrowser
 	    webbrowser.open( url['auth_url'] )
+	    
+	    # Wait to avoid webbrowser to corrupt raw_input
+	    rospy.sleep( rospy.Duration( 5 ) )
 
 	    # Enter pincode
-	    pincode = raw_input('Pincode: ')
+	    pincode = raw_input('Pincode: ').strip()
 
-	    auth_props = t.get_authorized_tokens( oauth_verifier = int(pincode) )
+	    auth_props = t.get_authorized_tokens(oauth_verifier = pincode)
 
 	    oauth_token = auth_props['oauth_token']
 	    oauth_token_secret = auth_props['oauth_token_secret']
 
 	    del t
 
-	rospy.loginfo('Using the following parameters for oauth: '
+	rospy.loginfo("Using the following parameters for oauth: "
 		    + 'key: [{key}], '.format(key = oauth_token)
 		    + 'secret: [{secret}]'.format(secret = oauth_token_secret))
 
@@ -102,7 +105,7 @@ class TwitterServer:
             oauth_token_secret = oauth_token_secret)
 	
 	result = self.t.verifyCredentials();
-	rospy.loginfo('Twitter connected as {name} (@{user})!'
+	rospy.loginfo("Twitter connected as {name} (@{user})!"
 		.format(name = result['name'], user = result['screen_name']))
 
 	# Stock screen name (used to show friendships)
@@ -131,20 +134,12 @@ class TwitterServer:
 
     # Tweet callback
     def post_cb(self, req):
-	rospy.logdebug('Received a tweet: ' + req.txt)
-        # Handle image
-    	if (req.image.height != 0):
-	    # Convert from ROS message using cv_bridge.
-            try:
-	        cv_image = self.bridge.imgmsg_to_cv( req.image, 
-		                        desired_encoding = 'passthrough')
-	    except CvBridgeError, e:
-      		rospy.logerr(e)
+	txt = req.text
+	rospy.logdebug("Received a tweet: " + txt)
 
-	    # Write to JPG with OpenCV
-	    path = "/tmp/pic_ul_{time}.png".format( time = time() )
-	    cv.SaveImage( path, cv_image)
-	    os.system('rm -f ' + path)
+        # If only one picture, use twitter upload
+    	if len(req.images) == 1:
+	    path = self.save_image( req.images[0] )
             
             if (req.reply_id == 0):
                 result = self.t.updateStatusWithMedia( file_ = path, 
@@ -152,11 +147,16 @@ class TwitterServer:
 	    else:
                 result = self.t.updateStatusWithMedia( file_ = path, 
 	    		status = req.text, in_reply_status_id = req.reply_id )
+	    
+	    os.system('rm -f ' + path)
 	
-        elif (req.reply_id == 0):
-            result = self.t.updateStatus( status = req.text )
+	elif len(req.images) != 0:
+	    txt +=  upload( req.images )
+	
+        if (req.reply_id == 0):
+            result = self.t.updateStatus( status = txt )
 	else:
-            result = self.t.updateStatus( status = req.text,
+            result = self.t.updateStatus( status = txt,
 	    			in_reply_to_status_id = req.reply_id )
 	
 	# Return false if call failed
@@ -174,7 +174,7 @@ class TwitterServer:
     
     # Does not raise an error if you are already following the user
     def follow_cb(self, req):
-	rospy.logdebug('Asked to follow:' + req.user)
+	rospy.logdebug("Asked to follow:" + req.user)
         result = self.t.createFriendship( screen_name = req.user )
 	if self.t.get_lastfunction_header('status') != '200 OK':
 	    return None
@@ -183,7 +183,7 @@ class TwitterServer:
     
     # Does not raise an error if you are not following the user
     def unfollow_cb(self, req):
-	rospy.logdebug('Asked to unfollow:' + req.user)
+	rospy.logdebug("Asked to unfollow:" + req.user)
         result = self.t.destroyFriendship( screen_name = req.user )
 	if self.t.get_lastfunction_header('status') != '200 OK':
 	    return None
@@ -192,26 +192,41 @@ class TwitterServer:
 
     # Send direct message.
     def post_dm_cb(self, req):
-	rospy.logdebug('Received DM to ' + req.user + ': ' + req.text)
+	rospy.logdebug("Received DM to " + req.user + ": " + req.text)
         
 	# First, check if you can dm the user
 	relation = self.t.showFriendship( 
 		source_screen_name = self.name, target_screen_name = req.user )
 	
 	if self.t.get_lastfunction_header('status') != '200 OK':
-	    rospy.logerr('Failed to get friendship information.')
+	    rospy.logerr("Failed to get friendship information.")
 	    return None
 
 	# If he can, send a direct message
-	if relation['source']['can-dm'] == 'true':
+	if relation['relationship']['source']['can_dm']:
+	    txt = req.text
+	    # Upload image to postimage.org using requests
+	    if len(req.images) != 0:
+		txt += self.upload( req.images ) 
             result = self.t.sendDirectMessage( 
-			screen_name = req.user, text = req.text )
-    # If he cant but was allowed to send a tweet instead, tweet with mention
-	elif replace_dm:
+			screen_name = req.user, text = txt )
+
+        # If he cant but was allowed to send a tweet instead, tweet with mention
+	elif self.replace_dm:
 	    rospy.logwarn("You can't send a direct message to " + req.user 
 	    	+ ". Sending a public tweet...")
-            result = self.t.updateStatus( 
-	    		status = "@" + req.user + " " + req.text )
+	    # One image ---> Twitter
+	    if len(req.images) == 1 :
+	        path = self.save_image( req.images[0] )
+                result = self.t.updateStatusWithMedia( file_ = path, 
+	    		status = req.text )
+                os.system('rm -rf ' + path)
+	    else:
+	    	status = '@' + req.user + ' ' + req.text
+		# Many images ---> postimage.org
+	        if len(req.images) != 0:
+		    status +=  upload( req.images )
+                result = self.t.updateStatus( status = status )
 	else:
 	    rospy.logwarn("You can't send a direct message to " + req.user)
 	    return None
@@ -238,8 +253,76 @@ class TwitterServer:
 	    if msg:
 	        return TimelineResponse( tweets = msg )
 	    else:
-	        rospy.logwarn(req.user + ' has no tweets in its timeline.')
+	        rospy.logwarn(req.user + " has no tweets in its timeline.")
 	        return TimelineResponse( )
+
+    # Upload array of sensor_msgs/Image to postimage.org and return link.
+    # Link is shortened if possible.
+    def upload(self, images):
+        files = {}
+	paths = [] # Keep paths stored to remove files later
+	# Construct files dict
+	i = 0
+	for image in images:
+	    paths.append( self.save_image( image ) )
+	    files['upload[{count}]'.format(count = i)] = open(paths[i], 'rb')
+	    i += 1
+	
+	# Post using requests
+	request = requests.post('http://postimage.org/index.php', 
+		files = files, params = {'optsize': 0, 'adult': 'no'})
+
+	# Cleanup saved files
+	for path in paths:
+	    os.system('rm -rf ' + path)
+
+	if not request.status_code in [301, 201, 200]:
+	    return " [FAILED UPLOAD]"
+
+	# Parse HTML page returned using beautifulsoup
+	soup = BeautifulSoup(request.text, 'html.parser')
+
+	# Find the image link: I hacked that by looking at raw html file
+	url = ""
+	if len(images) == 1:
+	    # 1 image : find first solo link containg 'postimg'
+	    for link in soup.find_all('a'):
+		if link.get('href').find('postimg') != -1:
+		    url = link.get('href')
+		    break
+	else:
+	    # Many images: find the option field for gallery url
+	    for option in soup.find_all('option'):
+		if option.get('value').find('gallery') != -1:
+		    url = option.get('value')
+		    break
+	
+	if not len(url):
+	    return " [FAILED PARSING OR UPLOAD]"
+		
+	# Shorten URL
+	request = requests.get( 'http://is.gd/create.php',
+	     params = {'format': 'simple', 'url': url} )
+
+	if request.status_code in [301, 201, 200]:
+	    return "  [" + request.text + "]"
+	else:
+	    return "  [" + url + "]"
+    
+    # Save a sensor_msgs/Image on /tmp and return the path
+    def save_image(self, image):
+        # Convert from ROS message using cv_bridge.
+        try:
+	    cv_image = self.bridge.imgmsg_to_cv( image, 
+				desired_encoding = 'passthrough')
+        except CvBridgeError, e:
+	    rospy.logerr(e)
+
+        # Write to JPG with OpenCV
+        path = "/tmp/pic_ul_{time}.png".format( time = time() )
+        cv.SaveImage( path, cv_image )
+	return path
+
     
     # Convert tweets from twitter structure to ROS structure
     # Args: statuses: An array of statuses returned by the api call.
@@ -260,16 +343,18 @@ class TwitterServer:
 	    if 'media' in s['entities']:
 	        for media in s['entities']['media']:
             	    if media['type'] == 'photo':
-			rospy.logdebug('Image detected: ' + media['media_url'])
-			# Download image
-	                r = urllib.URLopener()
+			rospy.logdebug("Image detected: " + media['media_url'])
+
+			# Download and write image using requests
 			path = '/tmp/pic_{num}_{time}.{ext}'.format( 
 				num = tweet.id, time = time(),
 				ext = media['media_url'].split('.')[-1] )
-		        r.retrieve( media['media_url'], path );
+			f = open(path, 'wb')
+			f.write( requests.get(media['media_url']).content )
+			f.close()
 
 		        # Read/Load with OpenCV
-		        cv_image = cv.LoadImage(path)
+		        cv_image = cv.LoadImage( path )
 		        os.system('rm -f ' + path)
 
 		        # Convert to ROS message using CV bridge.
@@ -308,6 +393,11 @@ class TwitterServer:
         # Timeline: To be changed when 1.1 is on.
 	response = self.t.getHomeTimeline( since_id = self.last_timeline, 
 						include_entities = True )
+	# Get headers just after function to avoid getting another call result
+	remaining = int( self.t.get_lastfunction_header( 
+						'x-rate-limit-remaining' ))
+	reset = int( self.t.get_lastfunction_header('x-rate-limit-reset') )
+
 	timeline_msg = self.process_tweets( response )
 	if len(timeline_msg.tweets):
 	    # Copy id of last tweet
@@ -316,10 +406,6 @@ class TwitterServer:
 	
 	# Reset timer to stick to rate limits 
 	# See: https://dev.twitter.com/docs/rate-limiting/1.1/limits
-	remaining = int( self.t.get_lastfunction_header( 
-						'x-rate-limit-remaining' ))
-	reset = int( self.t.get_lastfunction_header('x-rate-limit-reset') )
-	
 	if not remaining:
 	    rospy.logwarn("Twitter: Limit reached for timeline. Sleeping...")
 	    rospy.sleep( rospy.Duration( reset - rospy.Time.now().secs ) )
@@ -335,14 +421,15 @@ class TwitterServer:
 	# Mentions (exact same process)
 	response = self.t.getMentionsTimeline( since_id = self.last_mention,
 						include_entities = True )
-	mentions_msg = self.process_tweets( response )
-	if len(mentions_msg.tweets):
-	    self.last_mention = mentions_msg.tweets[0].id
-	    self.pub_mentions.publish( mentions_msg )
 	
 	remaining = int( self.t.get_lastfunction_header( 
 					'x-rate-limit-remaining' ) )
 	reset = int( self.t.get_lastfunction_header('x-rate-limit-reset') )
+
+	mentions_msg = self.process_tweets( response )
+	if len(mentions_msg.tweets):
+	    self.last_mention = mentions_msg.tweets[0].id
+	    self.pub_mentions.publish( mentions_msg )
 	
 	# Update rate
 	if not remaining:
@@ -360,14 +447,15 @@ class TwitterServer:
 	# Direct messages (make sure to not skip status)
 	response = self.t.getDirectMessages( since_id = self.last_dm, 
 					skip_status = False )
-	dm_msg = self.process_dm( response )
-	if len(dm_msg.tweets):
-	    self.last_dm = dm_msg.tweets[0].id
-	    self.pub_dm.publish( dm_msg )
 	
 	remaining = int( self.t.get_lastfunction_header( 
 						'x-rate-limit-remaining' ) )
 	reset = int( self.t.get_lastfunction_header( 'x-rate-limit-reset' ) )
+
+	dm_msg = self.process_dm( response )
+	if len(dm_msg.tweets):
+	    self.last_dm = dm_msg.tweets[0].id
+	    self.pub_dm.publish( dm_msg )
 	
 	# Update rate
 	if not remaining:
